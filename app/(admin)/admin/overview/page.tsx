b32aee276b778a3c0c6de9292196be638a1a730e
RafaelRMJesus <rafaelrocha191919@gmail.com>
2024-08-21 11:44:47 -0300

    fix: overview page does not match nextjs

--- app/(admin)/admin/overview/page.tsx
+++ app/(admin)/admin/overview/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import React from "react";
-import { ChevronRight, User } from "lucide-react";
-import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from "@/components/ui/carousel";
-import { contratos } from "@/mocks/contracts";
-import { ContractCard } from "@/components/contract-card";
 import { AdminBanner } from "@/components/admin-banner";
+import { BentoGridDemo } from "@/components/demo";
 
 const OverviewPage = () => {
   return (
@@ -20,71 +16,3 @@ const OverviewPage = () => {
 };
 
 export default OverviewPage;
-
-import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
-
-export function BentoGridDemo() {
-  return (
-    <BentoGrid className="max-w-full mx-auto">
-      {items.map((item, i) => (
-        <BentoGridItem
-          key={i}
-          title={item.title}
-          description={item.description}
-          header={item.header}
-          icon={item.icon}
-          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
-        />
-      ))}
-    </BentoGrid>
-  );
-}
-const Skeleton = () => (
-  <div className="flex w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100 text-neutral-900 items-center justify-center"> teste</div>
-);
-
-const items = [
-  {
-    title: "The Dawn of Innovation",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
-    icon: <User className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Digital Revolution",
-    description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
-    icon: <User className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Art of Design",
-    description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
-    icon: <User className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Power of Communication",
-    description:
-      "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
-    icon: <User className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Pursuit of Knowledge",
-    description: "Join the quest for understanding and enlightenment.",
-    header: <Skeleton />,
-    icon: <User className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Joy of Creation",
-    description: "Experience the thrill of bringing ideas to life.",
-    header: <Skeleton />,
-    icon: <User className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Spirit of Adventure",
-    description: "Embark on exciting journeys and thrilling discoveries.",
-    header: <Skeleton />,
-    icon: <User className="h-4 w-4 text-neutral-500" />,
-  },
-];
