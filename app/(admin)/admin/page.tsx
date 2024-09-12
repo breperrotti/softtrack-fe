f7ab1d9c3e8289a81f205e79aedf4217a884dce0
RafaelRMJesus <rafaelrocha191919@gmail.com>
2024-09-12 10:16:33 -0300

    fix: localstorage admin broke

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,18 +1,28 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { OverviewDashboard } from "@/components/overview-dashboard";
 import { AdminBanner } from "@/components/admin-banner";
 
 const AdminPage = () => {
-  return (
-    <div className="bg-gray-100 min-h-screen">
-      <AdminBanner title="Painel Administrador" />
-      <div className="p-4 sm:p-6 w-full">
-        <OverviewDashboard />
-      </div>
-    </div>
-  );
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) {
+        return null;
+    }
+
+    return (
+        <div className="bg-gray-100 min-h-screen">
+            <AdminBanner title="Painel Administrador" />
+            <div className="p-4 sm:p-6 w-full">
+                <OverviewDashboard />
+            </div>
+        </div>
+    );
 };
 
 export default AdminPage;
